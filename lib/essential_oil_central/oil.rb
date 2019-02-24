33b412b010c5bc83fd92d7af98e317fb358c06ad
Natasha Waller <natashawaller@Natashas-MacBook-Pro.local>
2019-02-24 18:44:31 -0500

    Add test data to oil.rb file to create sample objects.

--- lib/essential_oil_central/oil.rb
+++ lib/essential_oil_central/oil.rb
@@ -1,13 +1,12 @@
 #Code to create each oil object and set attributes and descriptions. 
 class EssentialOilCentral::Oil 
-#attr_accessor  :benefit, :aroma, :uses
-#attr_reader :name, :description
+  attr_accessor  :name, :description, :benefits, :aroma, :uses
 
 #@@all = [] 
 
-def self.collection
+  def self.collection
   #This should return the entire collection of oil instances 
-  puts <<-DOC
+    puts <<-DOC
         1. Lavender
         2. Frankincense
         3. Peppermint
@@ -21,7 +20,21 @@ def self.collection
         11. Bergamot
         12. Lemongrass
         DOC
-end
+        oil_1 = self.new
+        oil_1.name = "Lavender"
+        oil_1.description = "The most popular essential oil because of its versatility and well-liked floral, herbal aroma, lavender can be used in a variety of ways to inspire calm and relaxation."
+        oil_1.benefits = "Calming, relaxing"
+        oil_1.aroma = "Floral, herbal"
+        oil_1.uses = "Body oils, massage oils, cleansers, lotions, creams, body mists, perfumes, shampoos, conditioners, hair masks, hair serums, hair mists, room mists, diffusions "
+     
+        oil_2 = self.new
+        oil_2.name = "Frankincense"
+        oil_2.description = "Used extensively in incense and fine perfumes for thousands of years, frankincense essential oil is characterized by a resinous aroma and can bring a calming benefit to body care, diffusion and air freshening as well as grounding practices like meditation or yoga."
+        oil_2.benefits = "Meditative, grounding, calming"
+        oil_2.aroma = "Resinous, balsamic"
+        oil_2.uses = "Body oils, massage oils, body mists, perfumes, room mists, diffusions, meditation roll-ons and mists, yoga mists."
+       [oil_1, oil_2]   
+  end
 
 #Each type of oil will need to be initialized with a name and description
   #def initialize (name, description)
