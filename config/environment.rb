41db05a089e6aa49d73e84ddf8bbb42fcb0a9c3e
Natasha Waller <natashawaller@Natashas-MacBook-Pro.local>
2019-03-11 10:35:00 -0400

    Remove unnecessary notes from environment.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,5 @@
 #This is where you will require all of your required files to make the program run. 
-#Your console and your essential_oil_central files in bin will be dependent on this 
-#environment file instead of individual files
-#Try these with one dot 3/6
+
 
 require "open-uri" 
 require "nokogiri" 
